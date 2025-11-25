712d63c4ec2d4a1589e864d9db5400961460fc5f
Hameed Gul <hamidkhan31590@gmail.com>
2025-11-25 12:46:19 +0500

    reset setting

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -25,7 +25,6 @@ const Profile = () => {
         headline: userData.user.headline || "",
         username: userData.user.userName || "",
         email: userData.user.email || "",
-        image:userData.user.image
       });
     }
   }, [userData]);
